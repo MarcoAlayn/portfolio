21e50c5c48fd8c39e3a419b1ec9705da5a341699
MarcoAlayn <marc_rob92@hotmail.com>
2022-11-30 00:50:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <div id="containerRow">
+          <img src={logo} alt="logo" className="logo" />
+          <nav className="navbar">
+            <a href="/" className="link">
+              inicio
+            </a>
+            <a href="/" className="link">
+              sobre mi
+            </a>
+            <a href="/" className="link">
+              tecnologías
+            </a>
+            <a href="/" className="link">
+              experiencia
+            </a>
+            <a href="/" className="link">
+              contacto
+            </a>
+          </nav>
+        </div>
+
+        <div className="text">
+          <div id="containerColumn">
+            <h1>
+              Hola, Me llamo <br /> <span>Marco</span> y soy <br />
+              <span> Frontend Developer </span>
+            </h1>
+            <p>Bienvenid@ a mi portafolio</p>
+            <div id="containerRow" className="buttons ">
+              <a href="/">Descargar CV</a>
+              <a href="/">Contactame</a>
+            </div>
+          </div>
+        </div>
       </header>
+
+      <section className="about">sobre mi</section>
+      <section className="tecnologies">tecnologies</section>
+      <sections className="experience">experiencia</sections>
+      <section className="contact">contactame</section>
+      <footer className="footer">footer</footer>
     </div>
   );
 }
